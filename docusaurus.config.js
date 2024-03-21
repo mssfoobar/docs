326c3046b56dfaa9db8992bf86a7d2884157e040
Dois <kohehd@gmail.com>
2024-03-21 15:29:12 +0800

    docs: added new release

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,150 +2,181 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'AGIL Ops Hub',
-    tagline: 'Rapid Application Development for Agile Teams',
-    url: 'https://mssfoobar.github.io',
-    baseUrl: '/aoh-docs',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
+    title: "AGIL Ops Hub",
+    tagline: "Rapid Application Development for Agile Teams",
+    url: "https://mssfoobar.github.io",
+    baseUrl: "/aoh-docs",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'mssfoobar', // Usually your GitHub org/user name.
-    projectName: 'aoh-docs', // Usually your repo name.
-    deploymentBranch: 'gh-pages',
+    organizationName: "mssfoobar", // Usually your GitHub org/user name.
+    projectName: "aoh-docs", // Usually your repo name.
+    deploymentBranch: "gh-pages",
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
     // to replace "en" with "zh-Hans".
     i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
+        defaultLocale: "en",
+        locales: ["en"],
     },
 
     presets: [
         [
-            'classic',
+            "classic",
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/mssfoobar/aoh-docs/tree/main',
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    editUrl: "https://github.com/mssfoobar/aoh-docs/tree/main",
                     remarkPlugins: [math],
                     rehypePlugins: [katex],
                     docLayoutComponent: "@theme/DocPage",
                     docItemComponent: "@theme/ApiItem",
                 },
                 theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+                blog: {
+                    path: "blog",
+                    editUrl:
+                        "https://github.com/mssfoobar/aoh-docs/tree/main/blog",
+                    blogTitle: "AGIL Ops Hub Releases",
+                    routeBasePath: "releases",
+                    postsPerPage: 5,
+                    showReadingTime: true,
                 },
             }),
         ],
     ],
-    stylesheets: [{
-        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-        type: 'text/css',
-        integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-        crossorigin: 'anonymous',
-    },],
+    stylesheets: [
+        {
+            href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+            type: "text/css",
+            integrity:
+                "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+            crossorigin: "anonymous",
+        },
+    ],
 
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
             navbar: {
-                title: 'AGIL Ops Hub',
-                items: [{
-                    type: 'docSidebar',
-                    sidebarId: 'overview',
-                    position: 'left',
-                    label: "Overview"
-                },
-                {
-                    type: 'docSidebar',
-                    sidebarId: 'development',
-                    position: 'left',
-                    label: 'Development',
-                },
-                {
-                    type: 'docSidebar',
-                    sidebarId: 'deployment',
-                    position: 'left',
-                    label: 'Deployment',
-                }, {
-                    type: 'docSidebar',
-                    sidebarId: 'web',
-                    position: 'left',
-                    label: 'Web',
-                }, {
-                    type: 'docSidebar',
-                    sidebarId: 'iam',
-                    position: 'left',
-                    label: 'IAM',
-                }, {
-                    type: 'docSidebar',
-                    sidebarId: 'ucs',
-                    position: 'left',
-                    label: 'UCS',
-                },
-                {
-                    type: 'docSidebar',
-                    sidebarId: 'gis',
-                    position: 'left',
-                    label: 'GIS',
-                }, {
-                    type: 'docSidebar',
-                    sidebarId: 'rnr',
-                    position: 'left',
-                    label: 'RNR',
-                }, {
-                    type: 'docSidebar',
-                    sidebarId: 'wfe',
-                    position: 'left',
-                    label: 'WFE',
-                }, {
-                    type: 'docsVersionDropdown',
-                    position: 'right',
-                }, {
-                    href: 'https://github.com/mssfoobar',
-                    label: 'GitHub',
-                    position: 'right',
-                }, {
-                    type: 'search',
-                    position: 'right',
-                }
+                title: "AGIL Ops Hub",
+                items: [
+                    {
+                        to: "releases",
+                        label: "Releases",
+                        position: "left",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "overview",
+                        position: "left",
+                        label: "Overview",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "development",
+                        position: "left",
+                        label: "Development",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "deployment",
+                        position: "left",
+                        label: "Deployment",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "web",
+                        position: "left",
+                        label: "Web",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "iam",
+                        position: "left",
+                        label: "IAM",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "ucs",
+                        position: "left",
+                        label: "UCS",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "gis",
+                        position: "left",
+                        label: "GIS",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "rnr",
+                        position: "left",
+                        label: "RNR",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "wfe",
+                        position: "left",
+                        label: "WFE",
+                    },
+                    {
+                        type: "docsVersionDropdown",
+                        position: "right",
+                    },
+                    {
+                        href: "https://github.com/mssfoobar",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                    {
+                        type: "search",
+                        position: "right",
+                    },
                 ],
             },
             footer: {
-                style: 'dark',
-                links: [{
-                    title: 'Docs',
-                    items: [{
-                        label: 'GitHub Organisation',
-                        href: 'https://github.com/mssfoobar',
-                    }, {
-                        label: 'ST Engineering',
-                        href: 'https://www.stengg.com/',
-                    },],
-                },],
+                style: "dark",
+                links: [
+                    {
+                        title: "Docs",
+                        items: [
+                            {
+                                label: "GitHub Organisation",
+                                href: "https://github.com/mssfoobar",
+                            },
+                            {
+                                label: "ST Engineering",
+                                href: "https://www.stengg.com/",
+                            },
+                        ],
+                    },
+                ],
                 copyright: `Copyright © ${new Date().getFullYear()} | ST Engineering`,
             },
             colorMode: {
-                defaultMode: 'dark',
+                defaultMode: "dark",
                 disableSwitch: true,
                 respectPrefersColorScheme: false,
-            }
+            },
         }),
 
     plugins: [
         require.resolve("docusaurus-lunr-search"),
         [
-            'docusaurus-plugin-openapi-docs',
+            "docusaurus-plugin-openapi-docs",
             {
                 id: "apiDocs",
                 docsPluginId: "classic",
@@ -178,10 +209,10 @@ const config = {
                             groupPathsBy: "tag",
                         },
                     },
-                }
+                },
             },
         ],
-        "@docusaurus/plugin-ideal-image"
+        "@docusaurus/plugin-ideal-image",
     ],
     themes: ["docusaurus-theme-openapi-docs"],
 };
